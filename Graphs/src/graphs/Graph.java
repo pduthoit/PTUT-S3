b443ba6328e5ea8ca5577df10260d1b9a0401dcc
Arthur <arthur.erhel@hotmail.fr>
2016-01-02 16:56:28 +0100

    maj Graph

--- Graphs/src/graphs/Graph.java
+++ Graphs/src/graphs/Graph.java
@@ -2,7 +2,6 @@
 package graphs;
 
 import java.util.List;
-import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 
 public class Graph extends SingleGraph {
@@ -14,26 +13,26 @@ public class Graph extends SingleGraph {
     
     public Graph(String id,int nbrNoeud, int nbrLien) {
         super(id);
-        this.nbrNoeud = ajouterNNoeuds(this,nbrNoeud,listNoeud);
-        this.nbrLien = ajouterNliens(this,nbrLien,listLien);
+        this.listNoeud = ajouterNNoeuds(this,nbrNoeud,listNoeud);
+        this.listLien = ajouterNliens(this,nbrLien,listLien);
     }
     
-    public static int ajouterNNoeuds(Graph graph,int nbNoeuds,List<String> listNoeud){
+    public static List<String> ajouterNNoeuds(Graph graph,int nbNoeuds,List<String> listNoeud){
         for(int i=0;i<nbNoeuds;i++){
             graph.addNode("A"+i);
             listNoeud.add("A"+i);
         }
-        return nbNoeuds;
+        return listNoeud;
     }
         
-    public static int ajouterNliens(Graph graph, int nbLiens, List<String> listLien){
+    public static List<String> ajouterNliens(Graph graph, int nbLiens, List<String> listLien){
         for(int i =0;i<nbLiens;i++){
             int Noeud1 = (int) ((Math.random())%nbrNoeud);
             int Noeud2 = (int) ((Math.random())%nbrNoeud);
             graph.addEdge("0"+i, Noeud1, Noeud2);
             listLien.add("0"+i);
         }
-        return nbLiens;
+        return listLien;
     }
     
 }
