c2241ec672c235a38bac542c48bff20a6a2710fc
Arthur <arthur.erhel@hotmail.fr>
2016-01-02 16:58:44 +0100

    maj nom classe principale

---
+++ Graphs/src/graphs/principale.java
@@ -0,0 +1,8 @@
+package graphs;
+ 
+public class principale {
+        public static void main(String args[]) {
+                Graph graph = new Graph("graph1", 3, 2);
+                graph.display();
+        }
+}
