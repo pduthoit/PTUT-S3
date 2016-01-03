f5bd4bc2aad998d04741fe74cf27ca040de14ca7
Arthur <arthur.erhel@hotmail.fr>
2016-01-03 21:18:48 +0100

    maj class principale

--- Graphs/src/graphs/principale.java
+++ Graphs/src/graphs/principale.java
@@ -2,7 +2,8 @@ package graphs;
  
 public class principale {
         public static void main(String args[]) {
-                Graph graph = new Graph("graph1", 3, 2);
+                Graph graph = new Graph("graph1", 3, 3);
+/*interface graphique avec les boutons*/
                 graph.display();
         }
 }
