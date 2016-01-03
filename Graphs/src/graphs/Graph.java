34b5c9cf4dc4b532ec7c447ccbb671e6a1cc59b3
Arthur <arthur.erhel@hotmail.fr>
2016-01-03 21:18:29 +0100

    maj graph automatique

--- Graphs/src/graphs/Graph.java
+++ Graphs/src/graphs/Graph.java
@@ -8,31 +8,20 @@ public class Graph extends SingleGraph {
     
     private static int nbrNoeud;
     private static int nbrLien;
-    private static List<String> listNoeud;
+    private static List<String> listNoeud; //esssayer avec des hashmap
     private static List<String> listLien;
     
     public Graph(String id,int nbrNoeud, int nbrLien) {
         super(id);
-        this.listNoeud = ajouterNNoeuds(this,nbrNoeud,listNoeud);
-        this.listLien = ajouterNliens(this,nbrLien,listLien);
-    }
-    
-    public static List<String> ajouterNNoeuds(Graph graph,int nbNoeuds,List<String> listNoeud){
-        for(int i=0;i<nbNoeuds;i++){
-            graph.addNode("A"+i);
-            listNoeud.add("A"+i);
+        for(int i=0;i<nbrNoeud;i++){
+            addNode("N"+i);
+            //Graph.listNoeud.add("N"+i);
         }
-        return listNoeud;
-    }
-        
-    public static List<String> ajouterNliens(Graph graph, int nbLiens, List<String> listLien){
-        for(int i =0;i<nbLiens;i++){
-            int Noeud1 = (int) ((Math.random())%nbrNoeud);
-            int Noeud2 = (int) ((Math.random())%nbrNoeud);
-            graph.addEdge("0"+i, Noeud1, Noeud2);
-            listLien.add("0"+i);
+        for(int i =0;i<=nbrLien;i++){
+            String Noeud1 = "N" + ((int)(((Math.random())*10)%nbrNoeud));
+            String Noeud2 = "N" + ((int)(((Math.random())*10)%nbrNoeud));
+            addEdge("E"+i, Noeud1, Noeud2); /* ajouter un noeud doit se faire dans la classe principale, je dois trouver une solution.*/
+            //listLien.add("E"+i);
         }
-        return listLien;
     }
-    
 }
