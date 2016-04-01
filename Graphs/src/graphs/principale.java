99ddd4c11f5d1928b31b5f9ce8f547acb794f72b
PilouFR <pl.duthoit@gmail.com>
2016-04-01 15:42:51 +0200

    Delete useless directories

--- Graphs/src/graphs/principale.java
+++
@@ -1,9 +0,0 @@
-package graphs;
- 
-public class principale {
-        public static void main(String args[]) {
-                Graph graph = new Graph("graph1", 3, 3);
-/*interface graphique avec les boutons*/
-                graph.display();
-        }
-}
